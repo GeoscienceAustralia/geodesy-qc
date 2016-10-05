84a334f8daec20e210c9c1d923dc77dc5cda70f3
EC2 Default User <ec2-user@ip-172-31-11-60.ap-southeast-2.compute.internal>
2016-10-05 02:26:37 +0000

    Added base of Lambda function

---
+++ src/geodesy-qc/geodesy_qc.py
@@ -0,0 +1,11 @@
+from __future__ import print_function
+
+import urllib
+
+def lambda_handler(event, context):
+    # Get the file object and bucket names from the event
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.unquote_plus(
+        event['Records'][0]['s3']['object']['key']).decode('utf8')
+
+    session_id = context.aws_request_id

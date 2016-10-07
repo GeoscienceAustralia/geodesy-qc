b6ed416dd2ab32433a2c6cfcb306d0790aab61b1
EC2 Default User <ec2-user@ip-172-31-11-60.ap-southeast-2.compute.internal>
2016-10-07 00:35:24 +0000

    Updated version of Anubis

--- quality_check/quality_check.py
+++ quality_check/quality_check.py
@@ -3,10 +3,14 @@ from __future__ import print_function
 import urllib
 import os
 import datetime
+import boto3
+import botocore
 
 from BeautifulSoup import BeautifulSoup
 from lib.rinex_data import *
 from lib.executable import Executable
+
+S3 = boto3.client('s3')
 
 def lambda_handler(event, context):
     # Get the file object and bucket names from the event
@@ -32,21 +36,45 @@ def lambda_handler(event, context):
             key, bucket))
         raise err
 
+    # Get Nav File
     rinex = RINEXData(local_file)
-    
+
+    if rinex.marker_name == 'BRDC':
+        # Get list of files which haven't been QC'd
+        pass
+
+    else:
+        nav = getBRDCNav(bucket, rinex.start_time)
+
     generateQCConfig(rinex)
+
+
+def getBRDCNavFile(bucket, date):
+    # Floor date
+    date = datetime.datetime.strptime(date.strftime('%Y%j'), '%Y%j')
+    brdc = 'public/daily/{}/brdc{}0.16d.gz'.format(
+        date.strftime('%Y/%j'), date.strftime('%j'))
+
+    try:
+        response = S3.get_object(Bucket=bucket, Key=brdc)
+
+    except botocore.exceptions.ClientError as err:
+        if err.response['Error']['Code'] == 404:
+            print('Daily BRDC file does not yet exist for {}'.format(
+                date.strftime('%Y/%j'))
+            return
+
+    return response['Body'].read()
 
 
 def generateQCConfig(rinex):
     base = BeautifulSoup(open('anubis_base.cfg'))
 
     base.config.gen.beg.contents[0].replaceWith('"{}"'.format(
-        rinex.start_time.strftime('%Y-%m-%d %H:%M:%S')))
+        rinex.start_time.strftime('%Y-%m-%d 00:00:00')))
 
-    end_time = rinex.start_time + datetime.timedelta(
-        hours=23, minutes=59, seconds=59)
     base.config.gen.end.contents[0].replaceWith('"{}"'.format(
-        end_time.strftime('%Y-%m-%d %H:%M:%S')))
+        rinex.start_time.strftime('%Y-%m-%d 23:59:59')))
 
     base.config.gen.sys.contents[0].replaceWith('"GPS"')
     base.config.gen.int.contents[0].replaceWith('1')

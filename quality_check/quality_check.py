edab2bc5657057833d727370aa525a7567cf4c47
EC2 Default User <ec2-user@ip-172-31-11-60.ap-southeast-2.compute.internal>
2016-11-11 03:28:19 +0000

    Moved quality check from nav into quality check module, just add trigger with nav prefix to lambda

--- quality_check/quality_check.py
+++ quality_check/quality_check.py
@@ -43,6 +43,11 @@ def lambda_handler(event, context):
     key = urllib.unquote_plus(
         event['Records'][0]['s3']['object']['key']).decode('utf8')
 
+    status, file_type, data_type, year, day = key.split('/')[:5]
+    if file_type == 'nav':
+        triggerQCFromNav(year, day, context.function_name, bucket)
+        return
+
     # Use AWS request ID from context object for unique directory
     session_id = context.aws_request_id
     local_path = '/tmp/{}'.format(session_id)
@@ -239,3 +244,71 @@ def parseQCResult(filename):
             ES_CLIENT.index(index='quality_metrics', doc_type=types[_type], body=doc)
 
     return
+
+
+def triggerQCFromNav(year, day, function_name, bucket):
+    """Invoke a lambda function with PUT operations for all objects in archive 
+    for a given year and day
+    """
+    lambda_client = boto3.client('lambda')
+    lambdaTriggerParams = {
+        "Records": [
+            {
+                "eventTime": datetime.datetime.utcnow().isoformat() + 'Z',
+                "s3": {
+                    "object": {
+                        "key": "",
+                    },
+                    "bucket": {
+                        "name": "",
+                    },
+                    "s3SchemaVersion": "1.0"
+                },
+                "awsRegion": "ap-southeast-2",
+                "eventName": "ObjectCreated:Put",
+                "eventSource": "aws:s3"
+            }
+        ]
+    }
+
+    prefix = '/daily/obs/{}/{}/'.format(year, day)
+
+    for s3_obj in getKeys(bucket, ['public' + prefix, 'private' + prefix]):
+        request = lambdaTriggerParams
+        request['Records'][0]['s3']['object']['key'] = s3_obj
+        request['Records'][0]['s3']['bucket']['name'] = bucket
+
+        try:
+            lambda_client.invoke_async(
+                FunctionName=function_name,
+                InvokeArgs=json.dumps(request))
+
+        except Exception as err:
+            print('Invocation of Quality Check failed for {}\n{}'.format(
+                key, err))
+            pass
+
+
+def getKeys(bucket, prefixes):
+    """Get a list of S3 objects in a bucket with a given prefix
+
+    Paginates responses in case there are over 1000 entries
+
+    prefixes can be single prefix, or list of prefixes
+    """
+    s3_client = boto3.client('s3')
+
+    if type(prefixes) is not list:
+        prefixes = [prefixes]
+
+    keys = []
+    for prefix in prefixes:
+        paginator = s3_client.get_paginator('list_objects')
+        page_iterator = paginator.paginate(Bucket=bucket, Prefix=prefix)
+
+        for page in page_iterator:
+            if 'Contents' in page:
+                keys += [obj['Key'] for obj in page['Contents']]
+
+    return keys
+
